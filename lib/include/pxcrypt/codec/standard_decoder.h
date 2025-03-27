e9715f9e9eca01d1a04aa15653027422aeec9cb5
Christian Heimlich <chris@pcserenity.com>
2025-03-27 01:49:00 -0400

    Remove unused decoder error types

--- lib/include/pxcrypt/codec/standard_decoder.h
+++ lib/include/pxcrypt/codec/standard_decoder.h
@@ -47,10 +47,6 @@ public:
         DimensionMismatch,
         NotLargeEnough,
         InvalidMeta,
-        InvalidHeader,
-        LengthMismatch,
-        UnexpectedEnd,
-        ChecksumMismatch,
         SkimFailed
     };
 
